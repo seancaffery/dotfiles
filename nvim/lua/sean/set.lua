44186dde843502a7ac15ad81bbf5cc4893136424
Sean Caffery <sean@mryum.com>
2023-07-17 19:30:55 +1000

    Copy some settings from old config

--- nvim/lua/sean/set.lua
+++ nvim/lua/sean/set.lua
@@ -1,31 +1,34 @@
-vim.opt.guicursor = ""
+vim.opt.guicursor      = ""
 
-vim.opt.nu = true
+vim.opt.nu             = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
-vim.opt.expandtab = true
+vim.opt.tabstop        = 2
+vim.opt.softtabstop    = 2
+vim.opt.shiftwidth     = 2
+vim.opt.expandtab      = true
 
-vim.opt.smartindent = true
+vim.opt.smartindent    = true
 
-vim.opt.wrap = false
+vim.opt.wrap           = false
+vim.opt.backspace      = {"indent", "eol", "start"}           -- " (indent,eol,start) backspace through everything in insert mode
 
-vim.opt.swapfile = false
-vim.opt.backup = false
-vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.opt.undofile = true
+vim.opt.backupdir      = "~/.vim/_backup//" --" where to put backup files.
+vim.opt.directory      = "~/.vim/_temp//" --" where to put swap files.
+vim.opt.undodir        = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile       = true
 
-vim.opt.hlsearch = false
-vim.opt.incsearch = true
+vim.opt.hlsearch       = true
+vim.opt.incsearch      = true
+vim.opt.ignorecase     = true -- searches are case insensitive...
+vim.opt.smartcase      = true -- ... unless they contain at least one capital letter
 
-vim.opt.termguicolors = true
+vim.opt.termguicolors  = true
 
-vim.opt.scrolloff = 8
-vim.opt.signcolumn = "yes"
+vim.opt.scrolloff      = 8
+vim.opt.signcolumn     = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-
+vim.opt.list       = true -- " Show invisible characters
