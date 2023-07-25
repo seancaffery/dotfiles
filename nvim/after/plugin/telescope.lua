92a0fd0d49180c7d086e5249a43c26f925d5d4ba
Sean Caffery <sean@mryum.com>
2023-07-25 20:02:07 +1000

    Telescope 'smart' path display

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -4,7 +4,12 @@ vim.keymap.set('n', '<leader>of', function() builtin.oldfiles({ only_cwd = true 
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
+require('telescope').setup{
+  defaults = {
+    path_display = {'smart'}
+  }
+}
