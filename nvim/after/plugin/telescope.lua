f9a9f1afe09e81841f7ed69dee901cb6c27e1f23
Sean Caffery <sean@mryum.com>
2023-07-25 20:01:53 +1000

    Old files only current project

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-vim.keymap.set('n', '<leader>of', function() builtin.oldfiles({only_cwd=true}) end, {})
+vim.keymap.set('n', '<leader>of', function() builtin.oldfiles({ only_cwd = true }) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', function()
