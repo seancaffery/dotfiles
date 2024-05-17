7f0b423060c4b2e6c472d66b55affed0a0a2e8c9
Sean Caffery <sean@meandu.com>
2024-05-17 12:04:50 +1000

    Fixes for nvim 0.10

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -1,48 +1,5 @@
 local lsp = require('lsp-zero').preset({})
 
--- textDocument/diagnostic support until 0.10.0 is released
-_timers = {}
-local function setup_diagnostics(client, buffer)
-  if require("vim.lsp.diagnostic")._enable then
-    return
-  end
-
-  local diagnostic_handler = function()
-    local params = vim.lsp.util.make_text_document_params(buffer)
-    client.request("textDocument/diagnostic", { textDocument = params }, function(err, result)
-      if err then
-        local err_msg = string.format("diagnostics error - %s", vim.inspect(err))
-        vim.lsp.log.error(err_msg)
-      end
-      local diagnostic_items = {}
-      if result then
-        diagnostic_items = result.items
-      end
-      vim.lsp.diagnostic.on_publish_diagnostics(
-        nil,
-        vim.tbl_extend("keep", params, { diagnostics = diagnostic_items }),
-        { client_id = client.id }
-      )
-    end)
-  end
-
-  diagnostic_handler() -- to request diagnostics on buffer when first attaching
-
-  vim.api.nvim_buf_attach(buffer, false, {
-    on_lines = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-      _timers[buffer] = vim.fn.timer_start(200, diagnostic_handler)
-    end,
-    on_detach = function()
-      if _timers[buffer] then
-        vim.fn.timer_stop(_timers[buffer])
-      end
-    end,
-  })
-end
-
 -- adds ShowRubyDeps command to show dependencies in the quickfix list.
 -- add the `all` argument to show indirect dependencies as well
 local function add_ruby_deps_command(client, bufnr)
@@ -110,7 +67,7 @@ require('mason-lspconfig').setup({
     'efm',
     'gopls',
     'lua_ls',
-    'ruby_ls',
+    'ruby_lsp',
     'rust_analyzer',
     'terraformls',
     'tsserver',
@@ -139,10 +96,9 @@ require('mason-lspconfig').setup({
     lua_ls = function()
       require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
     end,
-    ruby_ls = function()
-      require('lspconfig').ruby_ls.setup({
+    ruby_lsp = function()
+      require('lspconfig').ruby_lsp.setup({
         on_attach = function(client, buffer)
-          setup_diagnostics(client, buffer)
           add_ruby_deps_command(client, buffer)
         end,
       })
