3e6929140db4e8d5efccfe9f6e8d9ed6f292856b
Sean Caffery <sean@meandu.com>
2024-07-22 11:45:05 +1000

    Formatting

--- nvim/lua/sean/remap.lua
+++ nvim/lua/sean/remap.lua
@@ -1,4 +1,3 @@
-
 vim.g.mapleader = ","
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("i", "jj", "<ESC>")
@@ -26,10 +25,10 @@ vim.keymap.set("n", "<C-l>", "<C-w>l")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 -- next greatest remap ever : asbjornHaland
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 -- This is going to get me cancelled
 vim.keymap.set("i", "<C-c>", "<Esc>")
@@ -50,6 +49,6 @@ vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/code/dotfiles/nvim/lua/sean/packer.
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>");
 
 vim.keymap.set("n", "<leader><leader>", function()
-    vim.cmd("so")
+  vim.cmd("so")
 end)
 
