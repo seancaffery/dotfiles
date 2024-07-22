43ea50f6745d2445b9279436daebbc3d605c4260
Sean Caffery <sean@meandu.com>
2024-07-22 11:43:54 +1000

    keymap to limit search to file dir

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -1,9 +1,11 @@
 local builtin = require('telescope.builtin')
+local utils = require("telescope.utils")
 vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set('n', '<leader>of', function() builtin.oldfiles({ only_cwd = true }) end, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
+vim.keymap.set('n', '<leader>pd', function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, {})
 vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 
 require('telescope').setup {
