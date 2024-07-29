7ae45b981c0614c9ad4454ea1bf8b9a84e881b09
Sean Caffery <sean@meandu.com>
2024-07-29 21:29:38 +1000

    Add c-p find files fallback

--- nvim/after/plugin/telescope.lua
+++ nvim/after/plugin/telescope.lua
@@ -2,7 +2,12 @@ local builtin = require('telescope.builtin')
 local utils = require("telescope.utils")
 vim.keymap.set('n', '<leader>pf', function() builtin.find_files({ hidden = true }) end, {})
 vim.keymap.set('n', '<leader>of', function() builtin.oldfiles({ only_cwd = true }) end, {})
-vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-p>', function()
+  -- fall back to file search if not in a git dir
+  if not pcall(builtin.git_files) then
+    builtin.find_files({ hidden = true })
+  end
+end, {})
 vim.keymap.set('n', '<leader>ps', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>pg', builtin.grep_string, {})
 vim.keymap.set('n', '<leader>pd', function() builtin.live_grep({ cwd = utils.buffer_dir() }) end, {})
