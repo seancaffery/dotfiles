8570a6c9d7eb514169f7ed4a5b227b498151d823
Sean Caffery <sean.caffery@ferocia.com.au>
2026-05-09 15:25:24 +1000

    link XDG config dirs in setup

--- setup.sh
+++ setup.sh
@@ -5,8 +5,10 @@ if [ -z "$DOTFILES_DIR" ]; then
   exit 1
 fi
 
-for file in $(ls $DOTFILES_DIR | grep -v 'setup.sh'); do
+for file in $(ls "$DOFILES_DIR" | grep -v 'setup.sh'); do
   ln -sf "$DOTFILES_DIR/$file" "$HOME/.$file"
   echo "Linking $HOME/.$file"
 done
 
+ln -sf "$DOTFILES_DIR/ghostty" "$HOME/.config/ghostty"
+ln -sf "$DOTFILES_DIR/nvim" "$HOME/.config/nvim"
