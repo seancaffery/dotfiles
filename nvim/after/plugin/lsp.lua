3c9c3862b2e0c5300d2846fa3436e113c3640be3
Sean Caffery <sean.caffery@ferocia.com.au>
2026-05-09 15:33:33 +1000

    Fix deprecation warning

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -207,7 +207,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("lsp", { clear = true }),
   callback = function(args)
     local client = vim.lsp.get_client_by_id(args.data.client_id)
-    if client.supports_method("textDocument/formatting") then
+    if client and client:supports_method("textDocument/formatting") then
       vim.api.nvim_create_autocmd("BufWritePre", {
         buffer = args.buf,
         callback = function()
